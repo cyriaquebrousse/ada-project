37d9ce3b6fc9d639fa3833dd8bbeb77a0d17ec48
Cyriaque Brousse <cyriaque.brousse@epfl.ch>
2017-01-06 15:55:27 +0100

    fixed bad behavior when calling with empty departure time

--- map/views.py
+++ map/views.py
@@ -11,7 +11,7 @@ def view_stations(request):
   """List of stations in Switzerland"""
   return render(request, 'map/stations.html')
 
-def view_isochrone(request, lat, lng, dep_time=1200):
+def view_isochrone(request, lat, lng, dep_time='1200'):
   """Isochrone map from given coordinates of starting point, and set departure time"""
   lat = float(lat)
   lng = float(lng)
