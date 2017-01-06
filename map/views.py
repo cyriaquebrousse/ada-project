2f08cebc557c30c8c107f31f16786d2d6ff55f47
Cyriaque Brousse <cyriaque.brousse@epfl.ch>
2017-01-06 14:57:41 +0100

    rule out invalid time objects by raising http400

--- map/views.py
+++ map/views.py
@@ -1,8 +1,8 @@
 #-*- coding: utf-8 -*-
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.shortcuts import render
 
-def home(reauest):
+def home(request):
   """dummy"""
   text = "Hello world!"
   return HttpResponse(text)
@@ -17,6 +17,14 @@ def view_stations(request):
 
 def view_isochrone(request, lat, lng, dep_time=1200):
   """Isochrone map from given coordinates of starting point, and set departure time"""
+  lat = float(lat)
+  lng = float(lng)
+  from datetime import datetime, time
+  try:
+    dep_time = datetime.strptime(dep_time, '%H%M').time()
+  except:
+    return HttpResponseBadRequest('400: Departure time could not be cast to time')
+
   return HttpResponse("""
     <h1>Dummy isochrone map</h1>
     <p>Map for coordinates ({0},{1}), with departure time set at {2}</p>
