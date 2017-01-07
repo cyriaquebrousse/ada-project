739cdc140957384f7d34a265d788f7295842f08d
Cyriaque Brousse <cyriaque.brousse@epfl.ch>
2017-01-07 16:43:28 +0100

    add Stop model

--- map/models.py
+++ map/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Stop(models.Model):
+  id = models.IntegerField(primary_key=True, unique=True)
+  name = models.CharField(max_length=255)
+  lat = models.FloatField()
+  lng = models.FloatField()
+
+  def __str__(self):
+    return '{0} {1}'.format(self.id, self.name)
