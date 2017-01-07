d594915c4100f466f9ccf99535780eb693b7f7b5
Cyriaque Brousse <cyriaque.brousse@epfl.ch>
2017-01-07 15:49:34 +0100

    MAP urls are named

--- map/urls.py
+++ map/urls.py
@@ -3,15 +3,16 @@ from . import views
 
 urlpatterns = [
   # dummy home page
-  url(r'home$', views.home),
+  url(r'home$', views.home, name='map_home'),
 
   # list of all stations
-  url(r'stations$', views.view_stations),
+  url(r'stations$', views.view_stations, name='map_stations'),
 
   # isochrone map
   url(
     r'isochrone/(?P<lat>[-+]?[0-9]*\.?[0-9]+),(?P<lng>[-+]?[0-9]*\.?[0-9]+)/(?P<dep_time>\d\d\d\d)/?$',
-    views.view_isochrone
+    views.view_isochrone,
+    name='map_isochrone'
   ),
   url(r'isochrone/(?P<lat>[-+]?[0-9]*\.?[0-9]+),(?P<lng>[-+]?[0-9]*\.?[0-9]+)/?$', views.view_isochrone)
 ]
